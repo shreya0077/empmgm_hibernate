227278a349e71302d86d4fc0ce7cb003e68b3709
shreya0077 <shreyachandne935@gmail.com>
2023-05-03 15:32:07 +0530

    second day

--- src/main/java/com/empmgm/App.java
+++ src/main/java/com/empmgm/App.java
@@ -7,28 +7,61 @@ package com.empmgm;
  */
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-       
-    	employee e1=new employee();
     	
-    	e1.setEmpId(12);
-    	e1.setEmpName("shreya");
-    	e1.setEmpLocation("Pune");
-    	e1.setEmpProject("java project");
-    
+//    	employee e1=new employee();
+//    	e1.setEmpId(15);
+//    	e1.setEmpName("rajesh");
+//    	e1.setEmpLocation("Goa");
+//    	e1.setEmpProject("react");
+    	
     	Configuration con=new Configuration().configure().addAnnotatedClass(employee.class);
     	SessionFactory sf=con.buildSessionFactory();
     	
     	Session ses=sf.openSession();
     	
     	Transaction tx=ses.beginTransaction();
-    	ses.save(e1);
+    	
+    	employee e2=new employee();
+    	//ses.save(e1)
+    	
+    	//e2=ses.get(employee.class,13); //to retrive data of user
+    	
+    	
+//    	String city="dubai";
+//    	e2=(employee)ses.createQuery("from employee where empLocation=:city")
+//    			.setParameter("city", city).uniqueResult();
+//    	
+//    	Query qu=ses.createQuery("from employee");
+//    	
+//    	List<employee> li=qu.list();
+//    	
+//    	for(employee e:li)
+//    		System.out.println(e.getEmpId()+" : "+e.getEmpName());
+   	
+//    	Query q=ses.createQuery("update employee set empLocation=:location where empId=:id")
+//    			.setParameter("location","banglore").setParameter("id",14);
+//    	int status=q.executeUpdate();
+//    	System.out.println(status);
+
+    Query qu=ses.createQuery("delete from employee where empId=:id").setParameter("id",14);
+    
+    System.out.println(qu.executeUpdate());
+    	
+    	
+    	
     	tx.commit();
     	
-    	System.out.println(e1);
+//    	System.out.println("employee name is "+e2.getEmpName());
+//    	
+//    	System.out.println(e2);
+   	
+    	
     	
     	
     }
